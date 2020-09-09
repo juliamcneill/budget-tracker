4e8dcc4e09563accf69d8bc3202b9aa32b331305
Julia McNeill <juliammcneill@gmail.com>
2020-09-09 01:17:35 -0400

    Update mongoose

--- database/index.js
+++ database/index.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-
 const connectionString = process.env.ATLAS_DATABASE_URL;
 
-mongoose.connect(connectionString, { useNewUrlParser: true });
+mongoose.connect(connectionString);
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "Connection error!"));
 db.once("open", () => {
   console.log("Connected!");
 });
